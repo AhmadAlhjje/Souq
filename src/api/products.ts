f12d4d64bddd682fb5ef3890641929d29fee8d53
AhmadAlhjje <ahmadobadaal7jje@gmail.com>
2025-08-28 14:28:02 +0300

    تعديل وحذف منتج معين

--- src/api/products.ts
+++ src/api/products.ts
@@ -9,6 +9,15 @@ export interface ProductData {
   images?: File[]; // مصفوفة صور يمكن إرسال صورة واحدة أو أكثر
 }
 
+// واجهة المنتج
+export interface ProductUpdateData {
+  name?: string;
+  description?: string;
+  price?: number;
+  stock_quantity?: number;
+  images?: File[] | null;
+}
+
 // إنشاء منتج جديد
 export const createProduct = async (productData: ProductData) => {
   const formData = new FormData();
@@ -31,5 +40,45 @@ export const createProduct = async (productData: ProductData) => {
     },
   });
 
+  return response.data;
+};
+
+// تعديل منتج معين
+export const updateProduct = async (
+  id: number | string,
+  productData: ProductUpdateData
+) => {
+  const formData = new FormData();
+
+  if (productData.name) formData.append("name", productData.name);
+  if (productData.description)
+    formData.append("description", productData.description);
+  if (productData.price !== undefined)
+    formData.append("price", String(productData.price));
+  if (productData.stock_quantity !== undefined)
+    formData.append("stock_quantity", String(productData.stock_quantity));
+
+  if (productData.images && productData.images.length > 0) {
+    productData.images.forEach((file, index) => {
+      formData.append(`images[${index}]`, file);
+    });
+  }
+  console.log("id is",id);
+  const response = await api.put(`/products/${id}`, formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+
+  return response.data;
+};
+
+// حذف منتج معين
+export const deleteProduct = async (id: number | string) => {
+  const response = await api.delete(`/products/${id}`);
+  return response.data;
+};
+
+// جلب منتج واحد حسب ID
+export const getProductById = async (id: number | string) => {
+  const response = await api.get(`/products/${id}`);
   return response.data;
 };
