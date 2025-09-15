993a877284c8038db2912dc020afeda08901b9b2
AhmadAlhjje <ahmadobadaal7jje@gmail.com>
2025-09-15 14:17:18 +0300

    تعديل حماية المسارات

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,34 +6,34 @@ export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   // حماية صفحات الـ admin
-  // if (pathname.startsWith("/admin")) {
-  //   if (!token) {
-  //     return NextResponse.redirect(new URL("/", request.url));
-  //   }
+  if (pathname.startsWith("/admin")) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
 
-  //   try {
-  //     const payload = JSON.parse(atob(token.split(".")[1]));
+    try {
+      const payload = JSON.parse(atob(token.split(".")[1]));
 
-  //     if (payload.exp && payload.exp < Date.now() / 1000) {
-  //       const response = NextResponse.redirect(new URL("/", request.url));
-  //       response.cookies.delete("access_token");
-  //       response.cookies.delete("refresh_token");
-  //       return response;
-  //     }
-  //     if (payload.role !== "merchant") {
-  //       return NextResponse.redirect(new URL("/", request.url));
-  //     }
+      if (payload.exp && payload.exp < Date.now() / 1000) {
+        const response = NextResponse.redirect(new URL("/", request.url));
+        response.cookies.delete("access_token");
+        response.cookies.delete("refresh_token");
+        return response;
+      }
+      if (payload.role !== "merchant") {
+        return NextResponse.redirect(new URL("/", request.url));
+      }
 
-  //     return NextResponse.next();
-  //   } catch (error) {
-  //     const response = NextResponse.redirect(new URL("/", request.url));
-  //     response.cookies.delete("access_token");
-  //     response.cookies.delete("refresh_token");
-  //     return response;
-  //   }
-  // }
+      return NextResponse.next();
+    } catch (error) {
+      const response = NextResponse.redirect(new URL("/", request.url));
+      response.cookies.delete("access_token");
+      response.cookies.delete("refresh_token");
+      return response;
+    }
+  }
 
-  // // إعادة التوجيه من الصفحة الرئيسية إذا كان المستخدم admin
+  // إعادة التوجيه من الصفحة الرئيسية إذا كان المستخدم admin
   // if (pathname === "/" && token) {
   //   try {
   //     const payload = JSON.parse(atob(token.split(".")[1]));
@@ -53,7 +53,7 @@ export function middleware(request: NextRequest) {
   //   }
   // }
 
-  // return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
